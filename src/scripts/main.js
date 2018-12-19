330cb8b43c7338ae70ac8c905a23a22cf756800e
Michael Savchuk <pixelrobin@hotmail.com>
2018-12-19 15:28:23 -0800

    Nav + index hero updates

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', function() {
 	const navSizeDiv = document.getElementById('nav-size');
 
 	const mobileNavHeight = navSizeDiv.clientHeight;
-	const menuItems = nav.querySelectorAll('a, button');
+	const menuItems = [...nav.querySelectorAll('a, button')];
 
 	let lastScrollPos = 0;
 
@@ -43,26 +43,83 @@ window.addEventListener('DOMContentLoaded', function() {
 	}
 
 	const navKeyHandler = e => {
+		if (toggle.getAttribute('aria-expanded') !== 'true') return;
+
 		const { keyCode, shiftKey } = e;
 
-		if (keyCode === 27) {
-			closeMenu();
-		} else if (keyCode === 9) {
-			const firstItem = menuItems[0];
-			const lastItem = menuItems[menuItems.length - 1];
-			const active = document.activeElement;
+		const KEYS = {
+			ESC: 27,
+			TAB: 9,
+			UP: 38,
+			DOWN: 40,
+			LEFT: 37,
+			RIGHT: 39,
+			HOME: 36,
+			END: 35
+		}
 
-			if (shiftKey) { // tab back
-				if (active === firstItem) {
-					e.preventDefault();
-					lastItem.focus();
-				}
-			} else { // tab forwards
-				if (active === lastItem) {
-					e.preventDefault();
-					firstItem.focus();
-				}
+		const tabUp = () => {
+			const index = menuItems.indexOf(document.activeElement);
+
+			if (index !== -1) {
+				if (index === 0) {
+					menuItems[menuItems.length - 1].focus();
+				} else menuItems[index - 1].focus();
 			}
+		}
+
+		const tabDown = () => {
+			const index = menuItems.indexOf(document.activeElement);
+
+			if (index !== -1) {
+				if (index === menuItems.length - 1) {
+					menuItems[0].focus();
+				} else menuItems[index + 1].focus();
+			}
+		}
+
+		switch (keyCode) {
+			case KEYS.ESC:
+				closeMenu();
+			break;
+
+			case KEYS.TAB:
+				const firstItem = menuItems[0];
+				const lastItem = menuItems[menuItems.length - 1];
+				const active = document.activeElement;
+
+				if (shiftKey) { // tab back
+					if (active === firstItem) {
+						e.preventDefault();
+						lastItem.focus();
+					}
+				} else { // tab forwards
+					if (active === lastItem) {
+						e.preventDefault();
+						firstItem.focus();
+					}
+				}
+			break;
+
+			case KEYS.UP:
+			case KEYS.LEFT:
+				tabUp();
+			break;
+
+			case KEYS.DOWN:
+			case KEYS.RIGHT:
+				tabDown();
+			break;
+
+			case KEYS.HOME:
+				menuItems[0].focus();
+			break;
+
+			case KEYS.END:
+				menuItems[menuItems.length - 1].focus();
+			break;
+
+			default: break;
 		}
 	}
 
